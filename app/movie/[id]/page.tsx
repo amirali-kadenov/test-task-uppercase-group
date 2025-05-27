3873fac34174a895d44a62402efea5cf43331ee5
Amirali <kadenov.a@mybuh.kz>
2025-05-27 19:44:57 +0500

    fix: movie page exports

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,7 +1,4 @@
-export {
-  MoviePage as default,
-  generateMetadata,
-  revalidate,
-  dynamicParams,
-  generateStaticParams,
-} from "@/pages/movie"
+export { MoviePage as default, generateStaticParams } from "@/pages/movie"
+export { generateMetadata } from "@/pages/movie/model/metadata"
+export const revalidate = 3600 // 1hour
+export const dynamicParams = true // isr
