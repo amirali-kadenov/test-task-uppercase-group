8669de71b052b28024e462e9404931f29749af0e
Amirali <kadenov.a@mybuh.kz>
2025-05-27 15:21:15 +0500

    refactor: adjust isEmpty

--- src/pages/movies/lib.ts
+++ src/pages/movies/lib.ts
@@ -4,8 +4,10 @@ import type { MoviesResponse } from "@/entities/movies/model/types"
 export const isEmpty = (
   movies: InfiniteData<MoviesResponse | null, unknown> | undefined,
 ) => {
-  return !movies?.pages.reduce(
+  const totalMovies = movies?.pages.reduce(
     (acc, page) => acc + (page?.Search?.length || 0),
     0,
   )
+
+  return totalMovies === 0
 }
